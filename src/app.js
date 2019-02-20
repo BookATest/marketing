66f7a3b38f91c0c69b8173ed63a389c1537dab00
Elliot <elliot@ayup.agency>
2019-02-20 16:39:09 +0000

    Add jump.js for scrolling

---
+++ src/app.js
@@ -0,0 +1,9 @@
+/* Import jump.js http://callmecavs.com/jump.js/ */
+import jump from "jump.js";
+
+/* Scroll down to first section */
+document
+  .getElementsByClassName("bat-hero__scroll-text")[0]
+  .addEventListener("click", function() {
+    jump(".bat-content-block", { a11y: true });
+  });
