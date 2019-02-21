453d976f348484eebe7e29f8fac335324edc526a
Elliot <elliot@ayup.agency>
2019-02-21 11:46:08 +0000

    Cleaned up icons mobile

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,7 +5,7 @@ require("mix-html-builder");
 mix
   .js("src/app.js", "dist")
   .sass("src/scss/style.scss", "dist/style.css")
+  .sourceMaps()
   .setPublicPath("dist")
-  .copy("src/index.html", "dist")
   .copy("src/assets", "dist/assets")
   .buildHtml({ inject: false });
