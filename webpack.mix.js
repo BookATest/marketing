acff6d4103c22baee0fc54ddb35f70f0f46c9464
Elliot <elliot@ayup.agency>
2019-02-21 11:04:04 +0000

    updated hover states and added ms prefix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,11 @@
-let mix = require('laravel-mix');
+let mix = require("laravel-mix");
 
-require('mix-html-builder');
+require("mix-html-builder");
 
-mix.js('src/app.js', 'dist')
-    .sass('src/scss/style.scss', 'dist/style.css')
-    .setPublicPath('dist')
-    .copy('src/index.html', 'dist')
-    .copy('src/assets', 'dist/assets')
-    .buildHtml();
+mix
+  .js("src/app.js", "dist")
+  .sass("src/scss/style.scss", "dist/style.css")
+  .setPublicPath("dist")
+  .copy("src/index.html", "dist")
+  .copy("src/assets", "dist/assets")
+  .buildHtml({ inject: false });
